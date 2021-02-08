1269615811187338ca1884dee46b7aeb7bb35a37
example <example@example.com>
2021-02-08 16:18:52 -0400

    inicios

---
+++ api/app/Controllers/Http/UploadController.js
@@ -0,0 +1,221 @@
+'use strict'
+
+const Helpers = use('Helpers')
+const mkdirp = use('mkdirp')
+const User = use("App/Models/User")
+const Producto = use("App/Models/Producto")
+const { validate } = use("Validator")
+const fs = require('fs')
+var randomize = require('randomatic');
+
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+/**
+ * Resourceful controller for interacting with uploads
+ */
+class UploadController {
+  async registrarProducto ({ request, response, auth }) {
+    let user = await auth.getUser()
+    let codeFile = randomize('Aa0', 30)
+    var dat = request.only(['dat'])
+    dat = JSON.parse(dat.dat)
+    const validation = await validate(dat, Producto.fieldValidationRules())
+    if (validation.fails()) {
+      response.unprocessableEntity(validation.messages())
+    } else {
+      const profilePic = request.file('files', {
+        types: ['image'],
+        size: '20mb'
+      })
+      if (Helpers.appRoot('storage/uploads/productos')) {
+        await profilePic.move(Helpers.appRoot('storage/uploads/productos'), {
+          name: codeFile,
+          overwrite: true
+        })
+      } else {
+        mkdirp.sync(`${__dirname}/storage/Excel`)
+      }
+      const data = { name: profilePic.fileName }
+      if (!profilePic.moved()) {
+        return profilePic.error()
+      } else {
+        dat.fileName = data.name
+      }
+      dat.proveedor_id = user._id.toString()
+      let guardar = await Producto.create(dat)
+      response.send(guardar)
+    }
+  }
+  async getFile({
+    params,
+    response
+  }) {
+    const fileName = params.filename
+    return fileName
+    response.download(Helpers.appRoot('storage/uploads') + `/${fileName}`)
+  }
+
+  /**
+   * Show a list of all uploads.
+   * GET uploads
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index({
+    request,
+    response,
+    view
+  }) {}
+
+  /**
+   * Render a form to be used for creating a new upload.
+   * GET uploads/create
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async create({
+    request,
+    response,
+    view
+  }) {}
+
+  /**
+   * Create/save a new upload.
+   * POST uploads
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store({
+    request,
+    response
+  }) {}
+
+  /**
+   * Display a single upload.
+   * GET uploads/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show({
+    params,
+    request,
+    response,
+    view
+  }) {}
+
+  /**
+   * Render a form to update an existing upload.
+   * GET uploads/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async edit({
+    params,
+    request,
+    response,
+    view
+  }) {}
+
+  /**
+   * Update upload details.
+   * PUT or PATCH uploads/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update({
+    params,
+    request,
+    response
+  }) {}
+
+  /**
+   * Delete a upload with id.
+   * DELETE uploads/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy({
+    params,
+    request,
+    response
+  }) {}
+
+  async uploadShopImage ({ auth, request }) {
+    let user = await auth.getUser()
+    let shop_id = request.input('shop_id')
+    let shop = await Shop.find(shop_id)
+    let dir = `storage/uploads/user_${user._id}/shop_${shop_id}/logo`
+    let showingDir = `user_${ user._id }-shop_${shop_id}-logo`
+
+    if (!shop) {
+      response.unprocessableEntity('El comercio no existe. Vuelva a cargar el formulario')
+    }
+
+    const shopImage = request.file('files', {
+      types: ['image'],
+      size: '2mb'
+    })
+    let fileName = `${new Date().getTime()}.${shopImage.subtype}`
+    await shopImage.move(Helpers.appRoot(dir), {
+      name: fileName,
+      overwrite: true
+    })
+    if (!shopImage.moved()) {
+      return shopImage.error()
+    }
+    if (shop.fileName) {
+      fs.unlinkSync(Helpers.appRoot(`storage/uploads/${shop.fileName.split('-').join('/')}`))
+    }
+    shop.fileName = `${showingDir}-${fileName}`
+    await shop.save()
+    return shop.fileName
+  }
+
+  async getFileByDirectoryProductos({ params, response, request }) {
+    const dir = params.file
+    response.download(Helpers.appRoot('storage/uploads/productos') + `/${dir}`)
+  }
+
+  async testFile ({ params, response, request }) {
+    response.download(Helpers.appRoot('storage/uploads/tiendaFiles/' + params.file))
+  }
+
+  async getFileByDirectoryNecesidad ({ params, request, response }) {
+    const dir = params.file
+    response.download(Helpers.appRoot('storage/uploads/necesidades') + `/${dir}`)
+  }
+
+  async getFileByDirectoryTienda ({ params, request, response }) {
+    const dir = params.file
+    response.download(Helpers.appRoot('storage/uploads/tiendaFiles') + `/${dir}`)
+  }
+
+  async getFileByDirectoryPerfil ({ params, request, response }) {
+    const dir = params.file
+    response.download(Helpers.appRoot('storage/uploads/perfil') + `/${dir}`)
+  }
+
+}
+
+module.exports = UploadController
